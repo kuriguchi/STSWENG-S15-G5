0617c0b945fa1d7766d02e455a1f2a799d52b330
jmrnd <jamarlancemiranda@gmail.com>
2024-03-06 20:16:16 +0800

    10_BE_AddedUpdateOrderTests

--- backend/__test__/order.test.js
+++ backend/__test__/order.test.js
@@ -67,4 +67,112 @@ describe('Order Controller', () => {
             }); 
         });
     });
+
+    describe('Updating an order', () => {
+        describe('Given a valid order ID and updated order details', () => {
+            beforeEach(() => {
+
+                req = { body: createRandomOrder() };
+                
+                req.params = { orderId: req.body.orderNum };
+
+                res = {
+                    json: sinon.spy(),
+                    status: sinon.stub().returnsThis()
+                };
+
+                expectedResult = req.body;
+            });
+
+            afterEach(() => {
+                if (orderStub) {
+                    orderStub.restore();
+                }
+            });
+
+            it('should update the order in the database', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').resolves(expectedResult);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledOnce(Order.updateOne);
+            });
+
+            it('should return status code 200', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').resolves(expectedResult);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.status, 200);
+            });
+
+            it('should return the updated order object', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').resolves(expectedResult);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.json, expectedResult);
+            });
+        });
+
+        describe('Given an invalid order ID', () => {
+            beforeEach(() => {
+
+                req = { body: createRandomOrder() };
+                
+                req.params = { orderId: req.body.orderNum };
+
+                res = {
+                    json: sinon.spy(),
+                    status: sinon.stub().returnsThis()
+                };
+            });
+
+            afterEach(() => {
+                if (orderStub) {
+                    orderStub.restore();
+                }
+            });
+
+            it('should return status code 404', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').resolves(null);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.status, 404);
+            });
+
+            it('should return an error message', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').resolves(null);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.json, { error: 404, message: 'Order not found.' });
+            });
+
+        });
+
+        describe('Given an error when updating the order', () => {
+            beforeEach(() => {
+                req = { body: createRandomOrder() };
+                
+                req.params = { orderId: req.body.orderNum };
+
+                res = {
+                    json: sinon.spy(),
+                    status: sinon.stub().returnsThis(),
+                    send: sinon.spy()
+                };
+            });
+
+            afterEach(() => {
+                if (orderStub) {
+                    orderStub.restore();
+                }
+            });
+
+            it('should return status code 500', async () => {
+                orderStub = sinon.stub(Order, 'updateOne').throws(new Error());
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.status, 500);
+            });
+
+            it('should return an error message', async () => {
+                const sampleError = new Error('Internal Server Error');
+                orderStub = sinon.stub(Order, 'updateOne').throws(sampleError);
+                await orderController.updateOrder(req, res);
+                sinon.assert.calledWith(res.send, sampleError);
+            });
+        });
+    });
 });
