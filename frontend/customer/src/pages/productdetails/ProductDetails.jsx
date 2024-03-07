79c48062c29b83bf369d8a68eed9f386e0950bfd
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-03-08 01:35:19 +0800

    Revert "Revert "17_FE_AddedImageCarouselTests""
    This reverts commit f9481f2b77a1ca5906778d6645077c1af60c4929.

--- frontend/customer/src/pages/productdetails/ProductDetails.jsx
+++ frontend/customer/src/pages/productdetails/ProductDetails.jsx
@@ -52,6 +52,7 @@ const ProductDetails = () => {
                             </div>
                         </div>
                         <div className="main-product-image-container">
+                            {/* image carousel to fix */}
                             <div className="main-product-image-wrapper">
                                 <img src={img6} alt="main product" className="main-product-image" />
                             </div>
